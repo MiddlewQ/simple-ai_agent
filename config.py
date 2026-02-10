8cee0e0f841f7ab6438aa970386fe04de05cc60e
Viktor Bergmark <viktor.bergmark@hotmail.com>
2026-02-10 17:02:39 +0100

    add: warning regarding modifying config

--- config.py
+++ config.py
@@ -2,6 +2,9 @@
 FILE_MAX_CHARS = 10000
 TIMEOUT_SECONDS = 30
 
+
+# Warning: careful changing this to avoid 
+# the AI deleting or modifying the wrong files 
+AI_WORKING_DIRECTORY = "./calculator" 
 AI_AGENT_MAX_LOOPS = 20
-AI_WORKING_DIRECTORY = "./calculator"
 AI_MODEL = "gemini-2.5-flash"
